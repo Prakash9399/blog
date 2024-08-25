b31a691e0992a29044360177bb2eaf37298f7932
Prakash9399 <prakashrajput9399@gmail.com>
2024-08-25 20:07:00 +0530

    model error fix

--- Models/blogModel.js
+++ Models/blogModel.js
@@ -17,6 +17,7 @@ const blogSchema = new mongoose.Schema({
   timestamps: true
 });
 
-const Blog = mongoose.model('Blog', blogSchema);
+// Check if the model is already compiled, otherwise compile it
+const Blog = mongoose.models.Blog || mongoose.model('Blog', blogSchema);
 
 module.exports = Blog;
